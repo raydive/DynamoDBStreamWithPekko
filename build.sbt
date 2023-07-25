29afa53557874232ebae32f2a6b8eb9d7939ec9f
raydive <raywassmas@gmail.com>
2023-07-25 18:27:20 +0900

    KCLSourceOnDynamoDBStreamsを使って、DynamoDB Streamの情報を取り出す

--- build.sbt
+++ build.sbt
@@ -2,12 +2,15 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.11"
 
+val PekkoVersion = "1.0.0"
 val PEKKO_KINESIS_VERSION = "1.0.5"
+
 
 lazy val root = (project in file("."))
   .settings(
     name := "DynamoDBStreamWithPekko",
     libraryDependencies ++= Seq(
+      "org.apache.pekko" %% "pekko-stream" % PekkoVersion,
       "com.github.j5ik2o" %% "pekko-kinesis-kcl" % PEKKO_KINESIS_VERSION,
       "com.github.j5ik2o" %% "pekko-kinesis-kcl-dynamodb-streams" % PEKKO_KINESIS_VERSION
     )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,53 @@
-class Main {
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
+import com.amazonaws.client.builder.AwsClientBuilder
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchClient
+import com.amazonaws.services.dynamodbv2.streamsadapter.AmazonDynamoDBStreamsAdapterClient
+import com.amazonaws.services.dynamodbv2.{AmazonDynamoDBClient, AmazonDynamoDBStreamsClient}
+import com.amazonaws.services.kinesis.clientlibrary.lib.worker.KinesisClientLibConfiguration
+import com.github.j5ik2o.ak.kcl.dyanmodb.streams.KCLSourceOnDynamoDBStreams
+import com.github.j5ik2o.ak.kcl.stage.CommittableRecord
+import org.apache.pekko.actor.ActorSystem
+import org.apache.pekko.stream._
+import org.apache.pekko.stream.scaladsl._
 
+import java.util.concurrent.Executors
+import scala.concurrent.ExecutionContext
+
+object Main extends App {
+  implicit val system: ActorSystem = ActorSystem("Demo")
+  val config = new KinesisClientLibConfiguration(
+    "DynamoDBStreamWithPekko",
+    "", // TODO テーブルからstreamarnの情報をとってくるようにする
+    DefaultAWSCredentialsProviderChain.getInstance(),
+    "worker"
+  )
+  val dynamoDBClient = AmazonDynamoDBClient.builder()
+    .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+      "http://localhost:8000", "us-west-2"
+    )).build()
+  val cloudwatchClient = AmazonCloudWatchClient.builder()
+    .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+      "http://localhost:4566", "us-west-2"
+    )).build()
+  val dynamoDBStreamsClient = AmazonDynamoDBStreamsClient.builder()
+    .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+      "http://localhost:8000", "us-west-2"
+    )).build()
+  val adapter = new AmazonDynamoDBStreamsAdapterClient(dynamoDBStreamsClient)
+
+  val executorService = Executors.newCachedThreadPool()
+  implicit val ctx = ExecutionContext.fromExecutorService(executorService)
+  KCLSourceOnDynamoDBStreams
+    .withoutCheckpoint(
+      kinesisClientLibConfiguration = config,
+      amazonDynamoDBStreamsAdapterClient = adapter,
+      amazonDynamoDB = dynamoDBClient,
+      amazonCloudWatchClientOpt = Some(cloudwatchClient),
+      execService = executorService,
+      metricsFactoryOpt = None
+    )
+    .viaMat(KillSwitches.single)(Keep.right)
+    .via(Flow[CommittableRecord].map { r => println(r.sequenceNumber); r })
+    .toMat(Sink.foreach(r => println(r.sequenceNumber)))(Keep.both)
+    .run()
 }
